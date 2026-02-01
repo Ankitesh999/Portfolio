d0bc6e29aa5c9b7e20fef4bbaae2862fb73b807a
ANKITESH TIWARI <138370094+Ankitesh999@users.noreply.github.com>
2026-02-01 13:10:06 +0530

    FIX production.py DATABASE CONFIG

--- portfolio/settings/production.py
+++ portfolio/settings/production.py
@@ -1,4 +1,5 @@
 import os
+import dj_database_url
 from .base import *
 
 DEBUG = True
@@ -15,14 +16,11 @@ ALLOWED_HOSTS = [
 ]
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('DB_NAME'),
-        'USER': os.environ.get('DB_USER'),
-        'PASSWORD': os.environ.get('DB_PASSWORD'),
-        'HOST': os.environ.get('DB_HOST'),
-        'PORT': os.environ.get('DB_PORT', '5432'),
-    }
+    "default": dj_database_url.parse(
+        os.environ.get("DATABASE_URL"),
+        conn_max_age=600,
+        ssl_require=True,
+    )
 }
 
 # Static files for production
