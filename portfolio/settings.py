6f0e1cffcc129f204c7ab79532ed415b4bd52ccd
ANKITESH TIWARI <138370094+Ankitesh999@users.noreply.github.com>
2026-02-11 18:34:57 +0530

    csrf trusted origins added to settings.py

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -4,4 +4,8 @@ ALLOWED_HOSTS = [
     ".onrender.com",
 ]
 
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.onrender.com",
+]
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'portfolio.settings.development')
