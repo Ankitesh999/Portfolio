197818b9450362e0d0854a52bb59e2277c7e76d2
ANKITESH TIWARI <138370094+Ankitesh999@users.noreply.github.com>
2026-02-11 18:39:34 +0530

    fixes the CSRF error when deployed on Render.com by adding the domain to the allowed hosts and setting the appropriate security headers.

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -8,4 +8,8 @@ CSRF_TRUSTED_ORIGINS = [
     "https://*.onrender.com",
 ]
 
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+USE_X_FORWARDED_HOST = True
+
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'portfolio.settings.development')
